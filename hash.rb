0fe714a0d366fd9095535e1b082008228df4225b
stm107 <44658004+stm107@users.noreply.github.com>
2019-04-13 11:32:44 -0400

    Moved hash methods into their own file.
    Started vile for validation methods.

---
+++ hash.rb
@@ -0,0 +1,22 @@
+# Checks command line arguments. If there isn't a single argument and it isn't the
+# the name of an existing file, return false. Else, return true.
+def check_args(args)
+    # If argument is an array and of length one
+    if args.is_a? Array and args.length == 1 and File.exist? args[0]
+        true
+    elsif args.is_a? String and File.exist? args
+        true
+    else
+        false
+    end     
+end
+
+# Calculates the hash for a block
+# Assumes the string entered does not include the final pipe and own hash
+def hash_block(block)
+  chars = block.unpack('U*') # Get array of UTF-8 values of each char in block
+  hash_calc = 0
+  chars.each { |c| hash_calc += ( ((c**3000) + (c**c) - (3**c)) * (7**c) )}
+  hash_calc %= 65536  # Modulo
+  hash_calc.to_s(16)  # Convert to string of hexadecimal representation
+end
--- verifier.rb
+++ verifier.rb
@@ -1,4 +1,6 @@
-require_relative 'methods'
+require_relative 'hash'
+require_relative 'validate_block'
+
 
 # Main file for the system
 exit 1 unless check_args ARGV
