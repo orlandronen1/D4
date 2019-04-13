b78b3763e5873fa268e8b22a041afd53b3dcbf94
stm107 <44658004+stm107@users.noreply.github.com>
2019-04-13 14:37:09 -0400

    Switched to using an array of hash maps for easier lookups

--- validate_block.rb
+++ validate_block.rb
@@ -1,44 +1,49 @@
-# Check that the block number on each line iterated by 1
-def create_array(text)
-  array=text.split("\n")
-  return array
+def create_maps(text)
+	maps = Array.new
+	array=text.split("\n")
+	array.each{|x|
+		# We'll need to do some validating of the blocks here
+		# Check the number of pipes
+		# Check the number of elements after splitting at pipe
+		# Check for nulls
+		elem = x.split('|')
+		kv_map = {id: elem[0], prev_hash: elem[1], transactions: elem[2], time_stamp: elem[3], hash: elem[4]}
+		maps.push(kv_map)
+	}
+	return maps
 end
 
-def verify_block_number(array)
-  array.each_with_index{|x, i| 
-  	return false if x[0].to_i != i }
-end
-
-def verify_block_pipes(array)
-  array.each{|x| puts false if x.count('|') != 4}
+def verify_block_number(maps)
+  maps.each_with_index{|x, i| 
+  	return false if x[:id].to_i != i }
 end
 
 # Split the array on | and get the second element of the array
 # For the first element, it should be 0
 # Verify first the format, so should contain 0-9a-f(lowercase)
 # Calculate hash of previous element and verify that they match.
-def verify_prev_hash_match(array)
+def verify_prev_hash_match(maps)
 	# special case for first line
-	elem = array[0].split('|')
-	puts false if elem[1].to_i != 0
+	return false if maps[0][:id].to_i != 0
 	#puts elem[1]
 
 	# now start at the 1st line
-	(1...array.length).each{|i| 
-		elem = array[i].split('|')
-		prev_elem = array[i-1].split('|')
+	(1...maps.length).each{|i| 
+		prev_hash = maps[i][:prev_hash]
+		curr_hash = maps[i-1][:hash]
+
 
 		# check that they match
-		return false if elem[1] != prev_elem[4]
+		return false if prev_hash != curr_hash
 
 		# check that the length of each is not more than 4
-		return false if elem[1].length > 4
-		return false if elem[1].length > 4
+		return false if prev_hash.length > 4
+		return false if curr_hash.length > 4
 
 		# check the formatting of the hash
-		elem[1].delete!("abcdef0123456789")
-		prev_elem[1].delete!("abcdef0123456789")
-		return false if elem[1].empty?
-		return false if prev_elem[1].empty?
+		prev_hash.delete!("abcdef0123456789")
+		curr_hash.delete!("abcdef0123456789")
+		return false if prev_hash.empty?
+		return false if curr_hash.empty?
 	}
 end
--- verifier.rb
+++ verifier.rb
@@ -7,11 +7,10 @@ exit 1 unless check_args ARGV
 
 # Open the file and pass the text to validate_block
 text=File.open(ARGV[0]).read
-block_array = create_array(text)
+maps = create_maps(text)
 
 # We can set the error if this returns false and print it out after. 
 # It should never return true, will only let us know if it fails.
-verify_block_number(block_array)
-verify_block_pipes(block_array)
-verify_prev_hash_match(block_array)
+verify_block_number(maps)
+verify_prev_hash_match(maps)
 
