b36865976e4cfb874b946d8602a4008bed54c0e7
Sean Mizerski <stm107@pitt.edu>
2019-04-14 18:45:46 -0400

    errors handled

--- verifier.rb
+++ verifier.rb
@@ -11,11 +11,12 @@ maps = create_maps(text)
 
 # We can set the error if this returns false and print it out after. 
 # It should never return true, will only let us know if it fails.
-verify_block_number(maps)
-verify_prev_hash_match(maps)
+exit 1 unless verify_block_number(maps)
+exit 1 unless verify_prev_hash_match(maps)
 balance = verify_transactions(maps)
-verify_time_stamp(maps)
-verify_hash(text)
-print_output(balance, maps.length - 1)
+exit 1 if balance == false
+exit 1 unless verify_time_stamp(maps)
+exit 1 unless verify_hash(text)
+exit 1 unless print_output(balance, maps.length - 1)
 
 
