d7df8a7e517a78f0fde77c4804812054cbb93acd
Sean Mizerski <stm107@pitt.edu>
2019-04-14 20:07:04 -0400

    rubocop on hash.rb and verifier.rb

--- hash.rb
+++ hash.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 # Checks command line arguments. If there isn't a single argument and it isn't the
 # the name of an existing file, return false. Else, return true.
 def check_args(args)
-    # If argument is an array and of length one
-    if args.is_a? Array and args.length == 1 and File.exist? args[0]
-        true
-    elsif args.is_a? String and File.exist? args
-        true
-    else
-      puts 'Usage: ruby verifier.rb <name_of_file>'
-      puts '     name_of_file = name of file to verify'
-      false
-    end     
+  # If argument is an array and of length one
+  if (args.is_a? Array) && (args.length == 1) && (File.exist? args[0])
+    true
+  elsif (args.is_a? String) && (File.exist? args)
+    true
+  else
+    puts 'Usage: ruby verifier.rb <name_of_file>'
+    puts '     name_of_file = name of file to verify'
+    false
+  end
 end
 
 # Calculates the hash for a block
@@ -18,7 +20,7 @@ end
 def hash_block(block)
   chars = block.unpack('U*') # Get array of UTF-8 values of each char in block
   hash_calc = 0
-  chars.each { |c| hash_calc += ( ((c**3000) + (c**c) - (3**c)) * (7**c) )}
-  hash_calc %= 65536  # Modulo
+  chars.each { |c| hash_calc += (((c**3000) + (c**c) - (3**c)) * (7**c)) }
+  hash_calc %= 65_536 # Modulo
   hash_calc.to_s(16)  # Convert to string of hexadecimal representation
 end
--- verifier.rb
+++ verifier.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 require_relative 'hash'
 require_relative 'validate_block'
-
 
 # Main file for the system
 exit 1 unless check_args ARGV
 
 # Open the file and pass the text to validate_block
-text=File.open(ARGV[0]).read
+text = File.open(ARGV[0]).read
 maps = create_maps(text)
 
-# We can set the error if this returns false and print it out after. 
+# We can set the error if this returns false and print it out after.
 # It should never return true, will only let us know if it fails.
 exit 1 unless verify_block_number(maps)
 exit 1 unless verify_prev_hash_match(maps)
@@ -18,5 +19,3 @@ exit 1 if balance == false
 exit 1 unless verify_time_stamp(maps)
 exit 1 unless verify_hash(text)
 exit 1 unless print_output(balance, maps.length - 1)
-
-
